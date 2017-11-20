483f0936e6e99a7a05980190f6277cbf24bbc8ef
akmerejf <akmerejf@gmail.com>
2017-11-20 10:30:53 -0300

    ajustes

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,12 +27,12 @@ namespace :deploy do
     on roles(:web), in: :sequence, wait: 5 do
       within release_path do
         execute :bundle, "--without development test"
+        execute :rake, 'db:migrate'
       end
     end
   end
   after :published, :install_dependencies
 end
-
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
